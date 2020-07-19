8db8d3188f16f6b6e48d4cf834635c82ef5a9da5
jackoyugi <oyugijack@gmail.com>
2020-07-19 13:31:05 +0300

    added vaccination properties, methods, getter and setters, and equals, hascode and tests for the methods

--- src/main/java/models/Vaccination.java
+++ src/main/java/models/Vaccination.java
@@ -1,4 +1,53 @@
 package models;
 
+import java.util.Objects;
+
 public class Vaccination {
+    private String vaccination_programme;
+    private int flock_number;
+    private int id;
+
+    public Vaccination(String vaccination_programme, int flock_number){
+        this.vaccination_programme = vaccination_programme;
+        this.flock_number = flock_number;
+    }
+
+    public String getVaccination_programme() {
+        return vaccination_programme;
+    }
+
+    public void setVaccination_programme(String vaccination_programme) {
+        this.vaccination_programme = vaccination_programme;
+    }
+
+    public int getFlock_number() {
+        return flock_number;
+    }
+
+    public void setFlock_number(int flock_number) {
+        this.flock_number = flock_number;
+    }
+
+    public int getId(int i) {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vaccination)) return false;
+        Vaccination that = (Vaccination) o;
+        return getId(1) == that.getId(1) &&
+                getVaccination_programme().equals(that.getVaccination_programme()) &&
+                getFlock_number()==(that.getFlock_number());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getVaccination_programme(), getFlock_number(), getId(1));
+    }
 }
---
+++ src/test/java/models/VaccinationTest.java
@@ -0,0 +1,50 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VaccinationTest {
+
+    @Test
+    public void getVaccination_programme() {
+        Vaccination testVaccine = setupVaccination();
+        assertEquals("poultry vaccine schedule", testVaccine.getVaccination_programme());
+    }
+
+    @Test
+    public void setVaccination_programme() {
+        Vaccination testVaccine = setupVaccination();
+        testVaccine.setVaccination_programme("poultry vaccine schedule");
+        assertNotEquals("kienyeji vaccine schedule", testVaccine.getVaccination_programme());
+    }
+
+    @Test
+    public void getFlock_number() {
+        Vaccination testVaccine = setupVaccination();
+        assertEquals(100, testVaccine.getFlock_number());
+    }
+
+    @Test
+    public void setFlock_number() {
+        Vaccination testVaccine = setupVaccination();
+        testVaccine.setFlock_number(100);
+        assertNotEquals(200, testVaccine.getFlock_number());
+    }
+
+    @Test
+    public void getId() {
+        Vaccination testVaccine = setupVaccination();
+        assertEquals(0, testVaccine.getId(0));
+    }
+
+    @Test
+    public void setId() {
+        Vaccination testVaccine = setupVaccination();
+        testVaccine.getId(0);
+        assertNotEquals(2, testVaccine.getId(2));
+    }
+    public Vaccination setupVaccination(){
+        return new Vaccination("poultry vaccine schedule", 100);
+    }
+}
