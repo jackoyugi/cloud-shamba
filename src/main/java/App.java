51b0637ddfd8fec97219d29357c76d2df9e4118d
jackoyugi <oyugijack@gmail.com>
2020-07-21 15:00:31 +0300

    added nav bar and stylings for the navbar

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,33 @@
+import models.Animal;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+import static spark.Spark.get;
+
 public class App {
+    public static void main(String[] args) {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        Integer port;
+
+        if (processBuilder.environment().get("PORT") != null) {
+            port = Integer.parseInt(processBuilder.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+        port(port);
+//        port(getHerokuAssignedPort());
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+    }
+
 
 }
