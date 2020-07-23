ba29de33e0f646bf1c1e5bc4b39a2c3208bddd85
Barsil Nzola <barsiljohn@gmail.com>
2020-07-23 15:40:20 +0300

    wrote tests to confirm diagnosis is saved into database correctly and can be manipulated and ran the tests

--- src/main/java/dao/Sql2oDiagnosisDao.java
+++ src/main/java/dao/Sql2oDiagnosisDao.java
@@ -1,4 +1,69 @@
 package dao;
 
-public class Sql2oDiagnosisDao {
+import models.Diagnosis;
+import org.sql2o.*;
+
+import java.util.List;
+
+public class Sql2oDiagnosisDao implements DiagnosisDao{
+
+    private final Sql2o sql2o;
+
+    public Sql2oDiagnosisDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(Diagnosis diagnosis) {
+        String sql = "INSERT INTO diagnosis (sex, age, breed, location, clinical_signs, herd_number, number_sick, number_dead, photo_url) VALUES (:sex, :age, :breed, :location, :clinical_signs, :herd_number, :number_sick, :number_dead, :photo_url)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(diagnosis)
+                    .executeUpdate()
+                    .getKey();
+            diagnosis.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public List<Diagnosis> getAll() {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM diagnosis")
+                    .executeAndFetch(Diagnosis.class);
+        }
+    }
+
+    @Override
+    public Diagnosis findById(int id) {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM diagnosis WHERE id = :id")
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Diagnosis.class);
+        }
+    }
+
+    @Override
+    public void deleteById(int id) {
+        String sql = "DELETE from diagnosis WHERE id = :id"; //raw sql
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void clearAllDiagnosis() {
+        String sql = "DELETE from diagnosis";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql).executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
 }
--- src/test/java/dao/Sql2oAnimalDaoTest.java
+++ src/test/java/dao/Sql2oAnimalDaoTest.java
@@ -11,12 +11,12 @@ public class Sql2oAnimalDaoTest {
     private Connection conn;
 
     @Before
-    public void setUp() throws Exception {
-        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
-        Sql2o sql2o = new Sql2o(connectionString, "", "");
-        animalDao = new Sql2oAnimalDao(sql2o);
-        conn = sql2o.open();
-    }
+public void setUp() throws Exception {
+    String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+    Sql2o sql2o = new Sql2o(connectionString, "", "");
+    animalDao = new Sql2oAnimalDao(sql2o);
+    conn = sql2o.open();
+}
 
     @After
     public void tearDown() throws Exception {
