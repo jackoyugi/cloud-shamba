038d059937f7e12ce003beee8f74cf230e66af4e
Barsil Nzola <barsiljohn@gmail.com>
2020-07-23 14:59:34 +0300

    created diagnosis table

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -5,3 +5,16 @@ id int PRIMARY KEY auto_increment,
 animal_type VARCHAR
 );
 
+CREATE TABLE IF NOT EXISTS diagnosis (
+id int PRIMARY KEY auto_increment,
+sex VARCHAR,
+age VARCHAR,
+breed VARCHAR,
+location VARCHAR,
+clinical_signs VARCHAR,
+herd_number INTEGER,
+number_sick INTEGER,
+number_dead INTEGER,
+photo_url BLOB
+);
+
